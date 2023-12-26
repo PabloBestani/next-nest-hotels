ce182c7e4212281c97ecd06b86fc43284c8592cf
Pablo Bestani <pablobestanimouk@gmail.com>
2023-12-26 10:53:17 -0300

    feat(routes): added route templates

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { HotelsModule } from './hotels/hotels.module';
+import { ReservationsModule } from './reservations/reservations.module';
+import { AuthModule } from './auth/auth.module';
+import { RoomTypeModule } from './room-type/room-type.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, HotelsModule, ReservationsModule, AuthModule, RoomTypeModule],
   controllers: [],
   providers: [],
 })
