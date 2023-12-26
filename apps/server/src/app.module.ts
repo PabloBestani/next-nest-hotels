a8dce1eb1859a24278ab0e3e0c233cb0f7241a70
Pablo Bestani <pablobestanimouk@gmail.com>
2023-12-26 12:19:40 -0300

    feat(db): added postgres DB and typeORM connection

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { HotelsModule } from './hotels/hotels.module';
 import { ReservationsModule } from './reservations/reservations.module';
@@ -6,7 +8,33 @@ import { AuthModule } from './auth/auth.module';
 import { RoomTypeModule } from './room-type/room-type.module';
 
 @Module({
-  imports: [UsersModule, HotelsModule, ReservationsModule, AuthModule, RoomTypeModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      host: process.env.POSTGRES_HOST,
+      port: process.env.POSTGRES_PORT ? +process.env.POSTGRES_PORT : 5432,
+      autoLoadEntities: true,
+      synchronize: true, //SETEAR EN FALSE al llevar a produ
+      ssl: process.env.POSTGRES_SSL === 'true',
+      extra: {
+        ssl:
+          process.env.POSTGRES_SSL === 'true'
+            ? {
+                rejectUnauthorized: false,
+              }
+            : null,
+      },
+    }),
+    UsersModule,
+    HotelsModule,
+    ReservationsModule,
+    AuthModule,
+    RoomTypeModule,
+  ],
   controllers: [],
   providers: [],
 })
