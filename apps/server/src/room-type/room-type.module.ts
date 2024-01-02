614b4b2e5d84fb9b7df841f13c8f72b2b5276c2f
Pablo Bestani <pablobestanimouk@gmail.com>
2024-01-02 15:49:48 -0300

    feat(room-type): added related table on get, put and patch

--- apps/server/src/room-type/room-type.module.ts
+++ apps/server/src/room-type/room-type.module.ts
@@ -3,9 +3,10 @@ import { RoomTypeService } from './room-type.service';
 import { RoomTypeController } from './room-type.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RoomType } from './entities/room-type.entity';
+import { HotelsModule } from 'src/hotels/hotels.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RoomType])],
+  imports: [TypeOrmModule.forFeature([RoomType]), HotelsModule],
   controllers: [RoomTypeController],
   providers: [RoomTypeService],
   exports: [RoomTypeService],
--- apps/server/src/room-type/room-type.service.ts
+++ apps/server/src/room-type/room-type.service.ts
@@ -1,32 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateRoomTypeDto } from './dto/create-room-type.dto';
 import { UpdateRoomTypeDto } from './dto/update-room-type.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { RoomType } from './entities/room-type.entity';
 import { Repository } from 'typeorm';
+import { HotelsService } from 'src/hotels/hotels.service';
 
 @Injectable()
 export class RoomTypeService {
   constructor(
     @InjectRepository(RoomType)
     private readonly roomTypesRepository: Repository<RoomType>,
+
+    private readonly hotelsService: HotelsService,
   ) {}
 
   async create(createRoomTypeDto: CreateRoomTypeDto) {
-    const newRoomType = this.roomTypesRepository.create(createRoomTypeDto);
-    return await this.roomTypesRepository.save(newRoomType);
+    try {
+      const hotel = await this.hotelsService.findOne(createRoomTypeDto.hotelId);
+
+      if (!hotel) throw new NotFoundException('Hotel not found');
+
+      const newRoomType = this.roomTypesRepository.create({
+        ...createRoomTypeDto,
+        hotel,
+      });
+
+      return await this.roomTypesRepository.save(newRoomType);
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
   }
 
   async findAll() {
-    return this.roomTypesRepository.find();
+    return this.roomTypesRepository.find({
+      relations: ['hotel'],
+    });
   }
 
   async findOne(id: number) {
-    return this.roomTypesRepository.findOneBy({ id });
+    return this.roomTypesRepository.findOne({
+      where: { id },
+      relations: ['hotel'],
+    });
   }
 
   async update(id: number, updateRoomTypeDto: UpdateRoomTypeDto) {
-    return this.roomTypesRepository.update(id, updateRoomTypeDto);
+    const roomType = await this.roomTypesRepository.findOneBy({ id });
+    if (!roomType) throw new NotFoundException('Room type not found');
+
+    const { hotelId, type, description, cost } = updateRoomTypeDto;
+
+    if (hotelId) {
+      const hotel = await this.hotelsService.findOne(hotelId);
+      if (!hotel) throw new NotFoundException('Hotel not found');
+      roomType.hotel = hotel;
+    }
+
+    if (type) roomType.type = type;
+    if (description) roomType.description = description;
+    if (cost) roomType.cost = cost;
+
+    return await this.roomTypesRepository.save(roomType);
   }
 
   async remove(id: number) {
